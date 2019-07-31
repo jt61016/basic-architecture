f9604a300afd901d0ec370be2190ce735bfb531f
jiangtao <jt61016@163.com>
2019-07-31 23:14:12 +0800

    initialize project

---
+++ demo-boot/src/main/java/com/demo/boot/ApplicationBoot.java
@@ -0,0 +1,15 @@
+package com.demo.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jiangtao
+ * @date 2019/7/31 下午9:09.
+ */
+@SpringBootApplication
+public class ApplicationBoot {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationBoot.class, args);
+    }
+}
